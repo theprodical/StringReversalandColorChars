c54d7bfc454d26dc2e860cbdf5e72225eb8aeb55
Jim Brumbaugh <doctordread@gmail.com>
2022-11-25 21:00:54 -0700

    Add project files.

---
+++ StringReversal/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+CS3910-C1 Module #4 Assignment 
+James Brumbaugh
+11/20/2022 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int vowelcount = 0;
+            Console.Write("Enter a String : ");
+            string originalString = Console.ReadLine();
+            string reverseString = string.Empty;
+            for (int i = originalString.Length - 1; i >= 0; i--)
+            {
+                reverseString += originalString[i];
+            }
+            for (int i = 0; i < reverseString.Length; i++)
+            {
+                string c = reverseString[i].ToString();
+                if ("aeiou".Contains(c))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                
+                Console.Write(reverseString[i]);
+            }        
+            Console.WriteLine();
+        }
+    }
+}
